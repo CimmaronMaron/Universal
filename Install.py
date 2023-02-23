df52ad09c1fefd1ab9d2ac2d9f8784c71c106598
CimmaronMaron <121487859+CimmaronMaron@users.noreply.github.com>
2023-02-23 20:59:47 +0200

    Add files via upload

---
+++ Install.py
@@ -0,0 +1,14 @@
+i = '''
+Welcome, this is my "PyFunc" module!
+get to work
+1. installation
+pip:
+pip install pyFunc
+conda/Anaconda:
+conda install pyFunc
+easy_install:
+easy_install pyFunc
+poetry:
+poetry add pyFunc
+'''
+print(i)
