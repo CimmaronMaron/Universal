54c8d22584b0805b908d5ef03b5438fd8f0ff4a3
CimmaronMaron <121487859+CimmaronMaron@users.noreply.github.com>
2023-02-25 17:40:32 +0200

    Update root.py

--- root.py
+++ root.py
@@ -4,7 +4,7 @@ from math import cos
 class root():
     def eqn(x):
         return x + cos(x)
-    def __init__(x):
+    def __init__(self, x):
         self.x=x
         y={'fjac': array([[-1.]]), 'fun': array([0.]), 'nfev': int(str(fix(x))+str(x[x-fix(x):-1])), 'qtf': array([-2.66786593e-13]), 'r': array([-1.67361202]), 'status': 1, success: True, 'x': array([-0.73908513])}
         self.y=y
