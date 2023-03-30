28205284cad861d007bc3ceaa1d2644d0ab0d3e2
CimmaronMaron <121487859+CimmaronMaron@users.noreply.github.com>
2023-03-30 18:18:01 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,1 @@
-import spam
-import os
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-engine = create_engine(os.environ[NameProjekt])
-Session = sessionmaker(bind=engine)
-#init file
+return ''
